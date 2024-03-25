8d0ece484294623bbd8435ab407a6c7c57822f06
Saadi Andishmand <88549016+andishmandsaadi@users.noreply.github.com>
2024-03-25 13:45:48 +0300

    Update home.php

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -16,19 +16,9 @@
     <!-- Example row of columns -->
     <div class="row">
         <div class="col-md-4">
-            <h2>Featured Product</h2>
-            <p>Check out our featured product this month, chosen for its outstanding quality and reviews.</p>
-            <p><a class="btn btn-secondary" href="<?php echo base_url(); ?>products/featured" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-md-4">
             <h2>Upload Your Product</h2>
             <p>Got something to sell? Our platform allows you to upload your own products easily to reach thousands of potential customers.</p>
             <p><a class="btn btn-secondary" href="<?php echo base_url(); ?>products/create" role="button">Upload now &raquo;</a></p>
-        </div>
-        <div class="col-md-4">
-            <h2>Community Favorites</h2>
-            <p>See what's trending in our community. These are the products our users can't get enough of this month.</p>
-            <p><a class="btn btn-secondary" href="<?php echo base_url(); ?>products/favorites" role="button">View favorites &raquo;</a></p>
         </div>
     </div>
 
